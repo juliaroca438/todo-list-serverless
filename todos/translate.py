163d8dc4a03bcc815086982f2ffccc62349d06f1
jroca <julia.roca@iecisa.com>
2021-01-18 13:05:41 +0100

    Add translate

--- todos/translate.py
+++ todos/translate.py
@@ -10,14 +10,13 @@ def translate(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
     # fetch todo from the database
-    resultTable = table.get_item(
+    result = table.get_item(
         Key={
             'id': event['pathParameters']['id']
         }
     )
-    print(resultTable)
-
-    itemJson = resultTable['Item']
+    itemJson = json.dumps(result['Item'],cls=decimalencoder.DecimalEncoder)
+    
     comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
 
     langsourceJson=comprehend.detect_dominant_language(Text=itemJson['text'], sort_keys=True, indent=4)
