e07fd477a1a89bdc3959035735ead19f6803645f
jroca <julia.roca@iecisa.com>
2021-01-18 15:18:45 +0100

    Add translate

--- todos/translate.py
+++ todos/translate.py
@@ -23,22 +23,27 @@ def translate(event, context):
 
     langsourceJson=comprehend.detect_dominant_language(Text=jsoncomprehen['text'])
     print(langsourceJson)
-    langSource=json.loads(langsourceJson['Languages'][0]['LanguageCode'])
-    targetLanguage=json.loads(event['pathParameters']['id']['lang'])
-
-    print(langSource)
-    
-    translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
-
-    resultTranslate = translate.translate_text(Text=jsoncomprehen['text'], 
-        SourceLanguageCode=langSource, TargetLanguageCode=targetLanguage)
-    
-    itemJson['text'] = resultTranslate
-
-    # create a response
+    #langSource=json.loads(langsourceJson['Languages'][0]['LanguageCode'])
+    #targetLanguage=json.loads(event['pathParameters']['id']['lang'])
+#
+    #print(langSource)
+    #
+    #translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+#
+    #resultTranslate = translate.translate_text(Text=jsoncomprehen['text'], 
+    #    SourceLanguageCode=langSource, TargetLanguageCode=targetLanguage)
+    #
+    #itemJson['text'] = resultTranslate
+#
+    ## create a response
+    #response = {
+    #    "statusCode": 200,
+    #    "body": json.dumps(itemJson,
+    #                       cls=decimalencoder.DecimalEncoder)
+    #}
     response = {
         "statusCode": 200,
-        "body": json.dumps(itemJson,
+        "body": json.dumps(langsourceJson,
                            cls=decimalencoder.DecimalEncoder)
     }
 
